a0b39c374acd44ca370b16a7ceedf7b310d82499
mhmakrostore-tech <mh.makrostore@gmail.com>
2026-09-19 18:05:05 -0400

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyAzN8MArYYQhIdhKkZwAI0EiBZla57l-Jg",
+  authDomain: "verkoop-curacao.firebaseapp.com",
+  projectId: "verkoop-curacao",
+  storageBucket: "verkoop-curacao.firebasestorage.app",
+  messagingSenderId: "469707957925",
+  appId: "1:469707957925:web:ab89dc2f2f04ea61188f4c"
+};
